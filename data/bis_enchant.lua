26554a7dda764966420fcae97471dd16403812c6
Warkdev <cedric.servais@outlook.com>
2021-12-14 20:02:44 +0100

    Update bis data

--- data/bis_enchant.lua
+++ data/bis_enchant.lua
@@ -1,3 +1,1 @@
 BIS_ENCHANT_LINKS = { };
-table.insert(BIS_ENCHANT_LINKS, {ClassId = 9, SpecId = 1, EnchantId = 44794, Priority = 1, Slot = 1 });
-table.insert(BIS_ENCHANT_LINKS, {ClassId = 9, SpecId = 1, EnchantId = 32872, Priority = 2, Slot = 1 });
